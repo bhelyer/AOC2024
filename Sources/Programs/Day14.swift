9734abaf3fb7c5a5cdc259610cf5726f83890429
Bernard Helyer <b.helyer@gmail.com>
2024-12-15 15:55:19 +1300

    Stub out day 14 part 1.

--- Sources/Programs/Day14.swift
+++ Sources/Programs/Day14.swift
@@ -5,10 +5,36 @@ class Day14: Program {
     }
     
     func part1(input: String) throws {
-        print("Day 14, part 1.")
+        let robots = parseRobots(from: input)
+        let dimensions = if robots.count > 20 {
+            // Real input.
+            Point(x: 101, y: 103)
+        } else {
+            // Sample input.
+            Point(x: 11, y: 7)
+        }
+        let finalState = simulate(initial: robots, mapSize: dimensions, forSeconds: 100)
+        print("Safety Factor=\(calculateSafetyFactor(robots: finalState))")
     }
     
     func part2(input: String) throws {
         print("Day 14, part 2.")
     }
+}
+
+struct Robot {
+    let pos: Point
+    let vel: Point
+}
+
+func parseRobots(from input: String) -> [Robot] {
+    return []
+}
+
+func simulate(initial robots: [Robot], mapSize: Point, forSeconds n: Int) -> [Robot] {
+    return robots
+}
+
+func calculateSafetyFactor(robots: [Robot]) -> Int {
+    return 0
 }
