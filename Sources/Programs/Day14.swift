83f82752af031371eb8e2d3190eb6efcfdcf116b
Bernard Helyer <b.helyer@gmail.com>
2024-12-15 16:09:35 +1300

    Day 14: Implement parseRobots.

--- Sources/Programs/Day14.swift
+++ Sources/Programs/Day14.swift
@@ -28,7 +28,50 @@ struct Robot {
 }
 
 func parseRobots(from input: String) -> [Robot] {
-    return []
+    let lines = input.split { $0.isNewline }
+    var robots: [Robot] = []
+
+    for line in lines {
+        guard let robot = parseRobot(line: line) else {
+            continue
+        }
+        robots.append(robot)
+    }
+
+    return robots
+}
+
+func parseRobot(line: String.SubSequence) -> Robot? {
+    let arr = [Character](line)
+    guard let firstEquals = arr.firstIndex(where: { $0 == "=" }) else {
+        return nil
+    }
+    guard let firstSpace = arr.firstIndex(where: { $0 == " "}) else {
+        return nil
+    }
+    guard let lastEquals = arr.lastIndex(where: { $0 == "=" }) else {
+        return nil
+    }
+    guard let pos = parsePoint(from: arr[firstEquals+1..<firstSpace]) else {
+        return nil
+    }
+    guard let vel = parsePoint(from: arr[lastEquals+1..<arr.count]) else {
+        return nil
+    }
+    return Robot(pos: pos, vel: vel)
+}
+
+func parsePoint(from arr: ArraySlice<Character>) -> Point? {
+    guard let comma = arr.firstIndex(where: { $0 == "," }) else {
+        return nil
+    }
+    guard let x = Int(String(arr[arr.startIndex..<comma])) else {
+        return nil
+    }
+    guard let y = Int(String(arr[comma+1..<arr.endIndex])) else {
+        return nil
+    }
+    return Point(x: x, y: y)
 }
 
 func simulate(initial robots: [Robot], mapSize: Point, forSeconds n: Int) -> [Robot] {
