db2ef8fce2d1bfba8db05e14eba624b6cd56fb75
Bernard Helyer <b.helyer@gmail.com>
2024-12-22 09:05:07 +1300

    Stub out day 21.

--- Sources/Program.swift
+++ Sources/Program.swift
@@ -35,6 +35,7 @@ func getProgram(byName programName: String) throws -> Program {
     case "day18": return Day18()
     case "day19": return Day19()
     case "day20": return Day20()
+    case "day21": return Day21()
     default: throw ProgramError.noSuchProgram
     }
 }
---
+++ Sources/Programs/Day21.swift
@@ -0,0 +1,5 @@
+class Day21: Program {
+    func run(input: String) async throws {
+        print("Day 21");
+    }
+}
