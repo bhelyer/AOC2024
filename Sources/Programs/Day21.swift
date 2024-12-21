d8caddad2afa9d09b701b1aac0aaf355e46ce131
Bernard Helyer <b.helyer@gmail.com>
2024-12-22 09:18:15 +1300

    Stub out structure of part 1.

--- Sources/Programs/Day21.swift
+++ Sources/Programs/Day21.swift
@@ -1,5 +1,27 @@
 class Day21: Program {
     func run(input: String) async throws {
-        print("Day 21");
+        print("Day 21, part 1 = \(part1(input))")
     }
+
+    private func part1(_ input: String) -> Int {
+        let codes = input.split { $0.isNewline }
+        var sum = 0
+        for code in codes {
+            sum += complexity(code)
+        }
+        return sum
+    }
+}
+
+private func complexity(_ code: Substring) -> Int {
+    return shortestSequence(code).count * numericPortion(code)
+}
+
+private func shortestSequence(_ code: Substring) -> [Character] {
+    return [">"]
+}
+
+/// Given `029A` return `29`.
+private func numericPortion(_ code: Substring) -> Int {
+    return Int(String([Character](code)[0..<3]))!
 }
