d59246e1d061ba83ff2602b3aea6d546bd213989
Bernard Helyer <b.helyer@gmail.com>
2024-12-23 08:28:05 +1300

    Day 22 part 1.

--- Sources/Programs/Day22.swift
+++ Sources/Programs/Day22.swift
@@ -1,15 +1,29 @@
 class Day22: Program {
     func run(input: String) async throws {
         let initialSecrets = input.split { $0.isNewline }.map { Int($0)! }
-        for var secret in initialSecrets {
-            for _ in 0...10 {
-                print(secret)
-                secret = advance(secret: secret)
+        var sum = 0
+        for secret in initialSecrets {
+            var advancedSecret = secret
+            for _ in 0..<2000 {
+                advancedSecret = advance(secret: advancedSecret)
             }
+            print("\(secret): \(advancedSecret)")
+            sum += advancedSecret
         }
+        print("Sum = \(sum)")
     }
 }
 
 private func advance(secret: Int) -> Int {
-    return secret
+    var secret = prune(mix(secret * 64, into: secret))
+    secret = prune(mix(secret / 32, into: secret))
+    return prune(mix(secret * 2048, into: secret))
+}
+
+private func mix(_ n: Int, into secret: Int) -> Int {
+    return secret ^ n
+}
+
+private func prune(_ secret: Int) -> Int {
+    return secret % 16777216
 }
