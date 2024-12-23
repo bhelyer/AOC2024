1d989b4f0d48f3e2cb70f09f126a273f9c7d639d
Bernard Helyer <b.helyer@gmail.com>
2024-12-24 10:16:12 +1300

    Day 23 part 1.

--- Sources/Programs/Day23.swift
+++ Sources/Programs/Day23.swift
@@ -1,27 +1,51 @@
 class Day23: Program {
+    var nodes: [Substring: [Substring]] = [:]
+    var pairs: [(Substring, Substring)] = []
+
     func run(input: String) async throws {
-        let connections = parseConnections(from: input)
-        print("Part 1 = \(part1(connections))")
+        parse(input)
+        print("Part 1 = \(part1())")
     }
     
-    private func part1(_ connections: [Connection]) -> Int {
-        print(connections)
-        return 0
+    private func parse(_ input: String) {
+        let lines = input.split { $0.isNewline }
+        for line in lines {
+            let halves = line.split { $0 == "-" }
+            guard halves.count == 2 else { continue }
+            
+            nodes[halves[0], default: []].append(halves[1])
+            nodes[halves[1], default: []].append(halves[0])
+            pairs.append((halves[0], halves[1]))
+        }
+    }
+    
+    private func part1() -> Int {
+        var trios =  Set<Trio>()
+        for (a, _) in nodes {
+            let aIsT = a.starts(with: /t/)
+            for (b, c) in pairs {
+                let bIsT = b.starts(with: /t/)
+                let cIsT = c.starts(with: /t/)
+                guard aIsT || bIsT || cIsT else { continue }
+                if nodes[b]!.contains(a) && nodes[c]!.contains(a) {
+                    var arr = [a, b, c]
+                    arr.sort()
+                    trios.insert(Trio(arr[0], arr[1], arr[2]))
+                }
+            }
+        }
+        return trios.count
     }
 }
 
-private struct Connection {
-    let first: Substring
-    let second: Substring
-}
-
-private func parseConnections(from input: String) -> [Connection] {
-    let lines = input.split { $0.isNewline }
-    var connections: [Connection] = []
-    for line in lines {
-        let halves = line.split { $0 == "-" }
-        guard halves.count == 2 else { continue }
-        connections.append(Connection(first: halves[0], second: halves[1]))
+private struct Trio: Hashable {
+    let a: Substring
+    let b: Substring
+    let c: Substring
+    
+    init(_ a: Substring, _ b: Substring, _ c: Substring) {
+        self.a = a
+        self.b = b
+        self.c = c
     }
-    return connections
 }
